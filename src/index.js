7cd9b08a27a19ef75675ed0dc2b28ebb945623b1
ldthanh0301 <ldthanh0301@gmail.com>
2021-08-28 20:47:01 +0700

    Add lib node-sass

--- src/index.js
+++ src/index.js
@@ -6,13 +6,23 @@ const handlebars  = require('express-handlebars')
 const app = express()
 const port = 3000
 
+// Khởi tạo handlebars
 app.engine('hbs', handlebars({
   extname: '.hbs'
 }));
-app.set('view engine', 'hbs');
+
+//  sử dụng view engine là hanlderbars
+app.set('view engine', 'hbs')
+
+// Chỉnh lại thu mục views cho đúng vối cấu trúc dự án
 app.set('views',path.join(__dirname,'resources/views'))
 
+// sử dụng morgan để hiển thị các logger 
 app.use(morgan('combined'))
+
+// set static folder 
+app.use( express.static(path.join(__dirname,'public')));
+
 app.get('/', (req, res) => {
   res.render('home');
 })
